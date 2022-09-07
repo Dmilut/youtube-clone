50fde65d266fb5d460b2fcd99a5dc3c1e38c3728
dmilut <d.lutcenko2017@gmail.com>
2022-09-06 20:45:55 -0700

    added model classes

--- backend/src/main/java/com/dmilut/youtubeclone/YoutubeCloneApplication.java
+++ backend/src/main/java/com/dmilut/youtubeclone/YoutubeCloneApplication.java
@@ -1,4 +1,4 @@
-package com.dmilut;
+package com.dmilut.youtubeclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ backend/src/main/java/com/dmilut/youtubeclone/model/VideoStatus.java
@@ -0,0 +1,7 @@
+package com.dmilut.youtubeclone.model;
+
+public enum VideoStatus {
+    PUBLIC,
+    PRIVATE,
+    UNLISTED
+}
