8098260fe12a27d6ad2ec6da2796e8433da5037a
dmilut <d.lutcenko2017@gmail.com>
2022-09-13 19:29:02 -0700

    refactoring

---
+++ backend/src/main/java/com/dmilut/youtubeclone/dto/VideoDto.java
@@ -0,0 +1,21 @@
+package com.dmilut.youtubeclone.dto;
+
+import com.dmilut.youtubeclone.model.VideoStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VideoDto {
+    private String id;
+    private String title;
+    private String description;
+    private Set<String> tags;
+    private String videoUrl;
+    private VideoStatus videoStatus;
+    private String thumbnailUrl;
+}
