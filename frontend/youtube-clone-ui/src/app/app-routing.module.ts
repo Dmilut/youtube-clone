ae5b96aa8fa2785086f95c434eec8d4681a60585
dmilut <d.lutcenko2017@gmail.com>
2022-09-13 19:26:34 -0700

    refactoring

--- frontend/youtube-clone-ui/src/app/app-routing.module.ts
+++ frontend/youtube-clone-ui/src/app/app-routing.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UploadVideoComponent } from './upload-video/upload-video.component';
-import { SaveVideoDetailsComponent } from './save-video-details/save-video-details.component';
+
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
 
 const routes: Routes = [
-  {
-    path: 'upload-video',
-    component: UploadVideoComponent,
-  },
-  {
-    path: 'save-video-details/:videoId',
-    component: SaveVideoDetailsComponent,
-  },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'user', component: BoardUserComponent },
+  { path: 'admin', component: BoardAdminComponent },
+  { path: 'mod', component: BoardModeratorComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- frontend/youtube-clone-ui/src/app/app.module.ts
+++ frontend/youtube-clone-ui/src/app/app.module.ts
@@ -1,66 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UploadVideoComponent } from './upload-video/upload-video.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { NgxFileDropModule } from 'ngx-file-drop';
-import { MatButtonModule } from '@angular/material/button';
-import { HeaderComponent } from './header/header.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { SaveVideoDetailsComponent } from './save-video-details/save-video-details.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatOptionModule } from '@angular/material/core';
-import { MatInputModule } from '@angular/material/input';
-import { MatChipsModule } from '@angular/material/chips';
-import { VgCoreModule } from '@videogular/ngx-videogular/core';
-import { VgControlsModule } from '@videogular/ngx-videogular/controls';
-import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
-import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { VideoPlayerComponent } from './video-player/video-player.component';
-import { AuthConfigModule } from './auth/auth-config.module';
-//import {SingleMediaPlayer} from './single-media-player';
+import { HomeComponent } from './home/home.component';
+
+import { authInterceptorProviders } from './helpers/auth.interceptor';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UploadVideoComponent,
-    HeaderComponent,
-    SaveVideoDetailsComponent,
-    VideoPlayerComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    ProfileComponent,
+    BoardUserComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     FormsModule,
-    HttpClientModule,
-    NgxFileDropModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatIconModule,
-    FlexLayoutModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatOptionModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    MatChipsModule,
-    VgCoreModule,
-    VgControlsModule,
-    VgOverlayPlayModule,
-    VgBufferingModule,
-    MatSnackBarModule,
-    AuthConfigModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent],
+  providers: [authInterceptorProviders],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
